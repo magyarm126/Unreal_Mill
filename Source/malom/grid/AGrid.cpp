71ea9594570d700b34c61ae5f7f7763639cf148d
Magyar Máté <magyarmate126@gmail.com>
2023-07-06 00:26:29 +0200

    Spawn name doesn't work

--- Source/malom/grid/AGrid.cpp
+++ Source/malom/grid/AGrid.cpp
@@ -3,6 +3,8 @@
 
 #include "AGrid.h"
 
+#include <string>
+
 AAGrid::AAGrid()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -11,21 +13,30 @@ AAGrid::AAGrid()
 void AAGrid::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	AGridItem* Baller = nullptr;
+
 	for (int i = 0; i < RowCount; ++i)
 	{
 		TArray<AGridItem*> TempArray = TArray<AGridItem*>();
 		for (int j = 0; j < ColumnCount; ++j)
 		{
 			FTransform ActorTransform = GetActorTransform();
-			ActorTransform.SetTranslation({(RowCount/2 - i) * OffsetVector.X, (ColumnCount/2 - j) * OffsetVector.Y, 0});
-			Baller = GetWorld()->SpawnActor<AGridItem>(GridItem, ActorTransform);
-			TempArray.Add(Baller);
+			ActorTransform.SetTranslation({
+				(RowCount / 2 - i) * OffsetVector.X, (ColumnCount / 2 - j) * OffsetVector.Y, 0
+			});
+
+			auto Params = FActorSpawnParameters();
+			std::string ClassName = std::string(TCHAR_TO_UTF8(*GridItem->GetClass()->GetFName().ToString()));
+			auto IndexString = ClassName + "[" + std::to_string(i) + "," + std::to_string(j) + "]";
+			Params.Name = IndexString.c_str();
+
+			UE_LOG(LogTemp, Warning, TEXT("Creating grid item: %s"), *Params.Name.ToString());
+			AGridItem* NewGridItem = GetWorld()->SpawnActor<AGridItem>(GridItem, ActorTransform, Params);
+
+			TempArray.Add(NewGridItem);
 		}
 		Matrix.Add(TempArray);
 	}
-	
+
 	for (int i = 0; i < Matrix.Num(); ++i)
 	{
 		for (int j = 0; j < Matrix[i].Num(); ++j)
@@ -34,4 +45,3 @@ void AAGrid::BeginPlay()
 		}
 	}
 }
-	
