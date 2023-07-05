48c2af8742835c0efbc2bd94552561a91e6beed1
Magyar Máté <magyarmate126@gmail.com>
2023-07-05 16:09:59 +0200

    Dynamic material coloring works

--- Source/malom/grid/AGrid.cpp
+++ Source/malom/grid/AGrid.cpp
@@ -14,14 +14,30 @@ void AAGrid::BeginPlay()
 
 	int RowCount = 3;
 	int ColumnCount = 3;
-	
+
+	AGridItem* Baller = nullptr;
 	for (int i = 0; i < RowCount; ++i)
 	{
 		for (int j = 0; j < ColumnCount; ++j)
 		{
 			FTransform ActorTransform = GetActorTransform();
 			ActorTransform.SetTranslation({(RowCount/2 - i) * OffsetVector.X, (ColumnCount/2 - j) * OffsetVector.Y, 0});
-			GetWorld()->SpawnActor<AGridItem>(GridItem, ActorTransform);	
+			Baller = GetWorld()->SpawnActor<AGridItem>(GridItem, ActorTransform);
+		}
+	}
+
+	auto a = Baller->GetComponents();
+	for (auto A : a)
+	{
+		if (A->GetClass() == UStaticMeshComponent::StaticClass())
+		{
+			UStaticMeshComponent* StaticMeshComponent = dynamic_cast<UStaticMeshComponent*>(A);
+			UMaterialInterface* MaterialInterface = StaticMeshComponent->GetMaterial(0);
+
+			UMaterialInstanceDynamic* DynamicMaterial = UMaterialInstanceDynamic::Create(MaterialInterface, this);
+			DynamicMaterial->SetVectorParameterValue(FName(TEXT("Color")), FLinearColor::Red);
+			
+			StaticMeshComponent->SetMaterial(0, DynamicMaterial);
 		}
 	}
 }
