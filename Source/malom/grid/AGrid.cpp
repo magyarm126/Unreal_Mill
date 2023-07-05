d22a8cd4afa8bbf26271b55b7ddab9231c1c25bc
Magyar Máté <magyarmate126@gmail.com>
2023-07-06 00:34:30 +0200

    Fixed labels

--- Source/malom/grid/AGrid.cpp
+++ Source/malom/grid/AGrid.cpp
@@ -23,14 +23,10 @@ void AAGrid::BeginPlay()
 			ActorTransform.SetTranslation({
 				(RowCount / 2 - i) * OffsetVector.X, (ColumnCount / 2 - j) * OffsetVector.Y, 0
 			});
+			AGridItem* NewGridItem = GetWorld()->SpawnActor<AGridItem>(GridItem, ActorTransform);
 
-			auto Params = FActorSpawnParameters();
-			std::string ClassName = std::string(TCHAR_TO_UTF8(*GridItem->GetClass()->GetFName().ToString()));
-			auto IndexString = ClassName + "[" + std::to_string(i) + "," + std::to_string(j) + "]";
-			Params.Name = IndexString.c_str();
-
-			UE_LOG(LogTemp, Warning, TEXT("Creating grid item: %s"), *Params.Name.ToString());
-			AGridItem* NewGridItem = GetWorld()->SpawnActor<AGridItem>(GridItem, ActorTransform, Params);
+			auto IndexString = "[" + std::to_string(i) + "," + std::to_string(j) + "]";
+			NewGridItem->SetActorLabel(NewGridItem->GetActorLabel() + IndexString.c_str());
 
 			TempArray.Add(NewGridItem);
 		}
